7c1a5b6fbec8e38f369595462aec59ade56604be
cvBesdelnik <101730217+cvBesdelnik@users.noreply.github.com>
2022-09-15 12:50:11 +0300

    First commit

---
+++ Boiko.js
@@ -0,0 +1,18 @@
+function courseBitcoin(dollars, bitcoin) {
+    return dollars / bitcoin;
+};
+
+console.log(courseBitcoin(100, 5));
+
+
+function howMuchDollars(bitcoin, course) {
+    return bitcoin * course;
+};
+
+console.log(howMuchDollars(10, 5));
+
+function iCanBuyBTC(dollars, course) {
+    return dollars / course;
+};
+
+console.log(iCanBuyBTC(10, 5));
